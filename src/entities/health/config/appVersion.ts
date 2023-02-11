e11772bc677bc8747e60176d862e34637713d426
sb <sb@sb.com>
2023-02-11 11:01:13 -0600

    docker fix, productions envsdata fix, basic ec2 ci/cd example added

--- src/entities/health/config/appVersion.ts
+++ src/entities/health/config/appVersion.ts
@@ -1,8 +1,8 @@
-export const commitID = `e45c2689602c7c4ca4ddc141edf4fb535013991e`;
+export const commitID = `0fc3967db8317bf016fc11a7292f96626d5a21c0`;
     
-  export const commitMssg = `starting pipeline config 10`;
+  export const commitMssg = `docker fix, productions envsdata fix, basic ec2 ci/cd example added`;
   
-  export const commitDate = `Sat Feb 11 2023 10:54:14 GMT-0600 (Central Standard Time)`;
+  export const commitDate = `Sat Feb 11 2023 11:01:13 GMT-0600 (Central Standard Time)`;
   
   export const commitAuthor = `sb`;
   
