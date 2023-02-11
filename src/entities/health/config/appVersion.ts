e372b00b36b72d5cc41fa919a6f227e2b232bfae
sb <sb@sb.com>
2023-02-10 18:11:09 -0600

    exact versions for packages and project folder structure

--- src/entities/health/config/appVersion.ts
+++ src/entities/health/config/appVersion.ts
@@ -1,8 +1,8 @@
-export const commitID = `4f11adca0644774da9830f00afb5aae9bb9a35d0`;
+export const commitID = `3b0856f86db7df0938ed9d207e4ff3ab626c2e01`;
     
   export const commitMssg = `exact versions for packages and project folder structure`;
   
-  export const commitDate = `Fri Feb 10 2023 18:08:46 GMT-0600 (Central Standard Time)`;
+  export const commitDate = `Fri Feb 10 2023 18:11:10 GMT-0600 (Central Standard Time)`;
   
   export const commitAuthor = `sb`;
   
