0804db5f94672f7c9e172a54e5a227c8167eef92
sb <sb@sb.com>
2023-02-10 18:08:46 -0600

    exact versions for packages and project folder structure

--- src/entities/health/config/appVersion.ts
+++ src/entities/health/config/appVersion.ts
@@ -1,8 +1,8 @@
-export const commitID = `9c55f9d3a12f72ae80239a9bc14e7b98ea2d42bc`;
+export const commitID = `4f11adca0644774da9830f00afb5aae9bb9a35d0`;
     
   export const commitMssg = `exact versions for packages and project folder structure`;
   
-  export const commitDate = `Fri Feb 10 2023 18:07:44 GMT-0600 (Central Standard Time)`;
+  export const commitDate = `Fri Feb 10 2023 18:08:46 GMT-0600 (Central Standard Time)`;
   
   export const commitAuthor = `sb`;
   
--- src/entities/health/config/envsData.ts
+++ src/entities/health/config/envsData.ts
@@ -1,10 +1,1 @@
-export const envsData= {
-	TIME_ZONE: `America/Monterrey`,
-	NODE_ENV: `development`,
-	DB_NAME: `forge-platform`,
-	DB_URL: `mongodb://127.0.0.1:27017`,
-	APP_PORT: `4000`,
-	DOMAIN: `localhost`,
-	CLIENT_URL: `http://localhost:3000`,
-	BACK_END_URL: `http://localhost:4000`,
-}
+export const envsData= {}
